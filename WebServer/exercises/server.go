ae496d4f6de0e8779f4e2379198858398944eecf
zhushh <zsh5318@gmail.com>
2017-03-28 04:20:53 +0800

    update

---
+++ WebServer/exercises/server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "io"
+    "math"
+    "math/rand"
+    "image"
+    "image/gif"
+    "image/color"
+    "strconv"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const(
+    whiteIndex = 0
+    blackIndex = 1
+)
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil {
+        log.Print(err)
+    }
+
+    var cycles = 5
+    var err error
+
+    for k, v := range r.Form {
+        if k == "cycles" {
+            cycles, err = strconv.Atoi(v[0])
+            if err != nil {
+                log.Print(err)
+            }
+            break
+        }
+    }
+
+    lissajous(w, cycles)
+}
+
+func lissajous(out io.Writer, cycles_value int) {
+    var cycles = cycles_value 
+    const(
+        // cycles  = cycles_value   // number of complete x oscillator revolutions
+        res     = 0.001 // angular resolution
+        size    = 100   // iamges canvas covers [-size...+size]
+        nframes = 64    // number of animation frames
+        delay   = 8     // delay between frames in 10ms units
+    )
+
+    freq := rand.Float64() * 3.0     // relative frequency of oscillator
+    anim := gif.GIF{LoopCount: nframes}
+    phase := 0.0    // phase difference
+
+    for i := 0; i < nframes; i++ {
+        rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+        img := image.NewPaletted(rect, palette)
+        for t := 0.0; t < float64(cycles)*2*math.Pi; t+= res {
+            x := math.Sin(t)
+            y := math.Sin(t*freq+phase)
+            img.SetColorIndex(size + int(x*size+0.5), size + int(y*size+0.5), blackIndex)
+        }
+        phase += 0.1
+        anim.Delay = append(anim.Delay, delay)
+        anim.Image = append(anim.Image, img)
+    }
+
+    gif.EncodeAll(out, &anim)
+}
+
